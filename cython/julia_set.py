6504ed931017d8e1a9a672bb1b1257ad50f8c4a1
flaviorv <frezendevaroto@gmail.com>
2025-02-02 22:07:24 -0300

    adding numpy that allocate memory in contiguous way

--- cython/julia_set.py
+++ cython/julia_set.py
@@ -1,9 +1,9 @@
 import time
-from dynamic_typing import calculate_z
 from without_cython import calculate_z_serial_purepython
-import static_typing
+import static_typing, with_numpy, dynamic_typing
+import numpy as np
 
-def calc_pure_python(desired_width, max_iterations):
+def calculate_cython_time(desired_width, max_iterations):
     x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
     c_real, c_imag = -0.62772, -.42193
 
@@ -32,16 +32,16 @@ def calc_pure_python(desired_width, max_iterations):
             cs.append(complex(c_real, c_imag))
 
     start_time = time.time()
-    output = calculate_z(max_iterations, zs, cs)
-    end_time = time.time()
-    secs = end_time - start_time
-    print("Dynamic typing took", secs, "seconds")
-
-    start_time = time.time()
     output = calculate_z_serial_purepython(max_iterations, zs, cs)
     end_time = time.time()
     secs = end_time - start_time
     print("Without cython took", secs, "seconds")
+
+    start_time = time.time()
+    output = dynamic_typing.calculate_z(max_iterations, zs, cs)
+    end_time = time.time()
+    secs = end_time - start_time
+    print("Dynamic typing took", secs, "seconds")
 
     start_time = time.time()
     output = static_typing.calculate_z(max_iterations, zs, cs)
@@ -49,7 +49,15 @@ def calc_pure_python(desired_width, max_iterations):
     secs = end_time - start_time
     print("Static typing took", secs, "seconds")
 
+    start_time = time.time()
+    _zs = np.array(zs)
+    _cs = np.array(cs)
+    output = with_numpy.calculate_z(max_iterations, _zs, _cs)
+    end_time = time.time()
+    secs = end_time - start_time
+    print("Static typing took", secs, "seconds")
+
     assert sum(output) == 33219980
 
 if __name__ == "__main__":
-    calc_pure_python(desired_width=1000, max_iterations=300)
+    calculate_cython_time(desired_width=1000, max_iterations=300)
